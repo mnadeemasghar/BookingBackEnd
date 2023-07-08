f6e7f290b720076b711be70e37133d918de5f4e5
Naadeem <mnadeem24434@gmail.com>
2023-07-08 23:50:45 +0500

    remaining navbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,21 +3,7 @@
 
     <!-- Content Start -->
     <div class="content">
-        <!-- Navbar Start -->
-        <nav class="navbar navbar-expand bg-secondary navbar-dark sticky-top px-4 py-0">
-            <a href="{{route('home')}}" class="navbar-brand d-flex d-lg-none me-4">
-                <h2 class="text-primary mb-0"><i class="fa fa-user-edit"></i></h2>
-            </a>
-            <a href="#" class="sidebar-toggler flex-shrink-0">
-                <i class="fa fa-bars"></i>
-            </a>
-            <form class="d-none d-md-flex ms-4">
-                <input class="form-control bg-dark border-0" type="search" placeholder="Search">
-            </form>
-            <div class="navbar-nav align-items-center ms-auto">
-            </div>
-        </nav>
-        <!-- Navbar End -->
+        @include('navbar')
 
 
         <!-- Sale & Revenue Start -->
